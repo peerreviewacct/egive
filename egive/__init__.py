5324a778692ab0253c53635731cbe2abea482c75
peerreviewacct <matthew.p.baucum@gmail.com>
2026-01-28 14:45:30 -0600

    Update __init__.py

--- egive/__init__.py
+++ egive/__init__.py
@@ -7,4 +7,4 @@ __version__ = "0.1.0"
 from .core import run_egive
 from .utils import neg_auc, rmse, mse, mae, retrieve_quantiles, get_neighborhood_points, logit_catch_singular, propensity_filters_and_weights, pdp_propensity_filters_and_weights, cutpoints, feature_importance_scores
 
-__all__ = ['egive', 'neg_auc', 'rmse', 'mse', 'mae','retrieve_quantiles', 'get_neighborhood_points', 'logit_catch_singular', 'propensity_filters_and_weights', 'pdp_propensity_filters_and_weights', 'cutpoints', 'feature_importance_scores']
+__all__ = ['egive', 'run_egive', 'neg_auc', 'rmse', 'mse', 'mae','retrieve_quantiles', 'get_neighborhood_points', 'logit_catch_singular', 'propensity_filters_and_weights', 'pdp_propensity_filters_and_weights', 'cutpoints', 'feature_importance_scores']
