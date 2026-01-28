7751c725f55ca9d31408977f700e71c99559ffb4
peerreviewacct <matthew.p.baucum@gmail.com>
2026-01-28 14:55:14 -0600

    Update __init__.py

--- egive/__init__.py
+++ egive/__init__.py
@@ -5,6 +5,6 @@ egive: A Python package for EGIVE, an efficient variable importance and interact
 __version__ = "0.1.0"
 
 from .core import run_egive
-from .utils import neg_auc, rmse, mse, mae, retrieve_quantiles, get_neighborhood_points, logit_catch_singular, propensity_filters_and_weights, pdp_propensity_filters_and_weights, cutpoints, feature_importance_scores
+from .utils import neg_auc, rmse, mse, mae, retrieve_quantiles, get_neighborhood_points, logit_catch_singular, propensity_filters_and_weights, pdp_propensity_filters_and_weights, cutpoints, feature_importance_scores, egive_center_pdp, egive_append_threeway_scores
 
-__all__ = ['egive', 'run_egive', 'neg_auc', 'rmse', 'mse', 'mae','retrieve_quantiles', 'get_neighborhood_points', 'logit_catch_singular', 'propensity_filters_and_weights', 'pdp_propensity_filters_and_weights', 'cutpoints', 'feature_importance_scores']
+__all__ = ['egive', 'run_egive', 'neg_auc', 'rmse', 'mse', 'mae','retrieve_quantiles', 'get_neighborhood_points', 'logit_catch_singular', 'propensity_filters_and_weights', 'pdp_propensity_filters_and_weights', 'cutpoints', 'feature_importance_scores', 'egive_center_pdp', 'egive_append_threeway_scores']
