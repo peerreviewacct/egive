b91c5c7fb261291f389dc81dad04fcd160745715
peerreviewacct <matthew.p.baucum@gmail.com>
2026-02-10 22:22:09 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="egive",
-    version="0.1.0",
+    version="0.1.1",
     author="Anonymous for peer review",  
     author_email="",  
     description="A Python package for EGIVE, an efficient variable importance and interaction detection method for black-box ML models",
